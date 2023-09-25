859c5d8c83610687606875d3e2de6eed2e541ae0
Magdalena Dalka <victoriaphotographypreston@gmail.com>
2023-09-25 19:11:08 +0100

    funkcja

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let firstNumber = 5;
+let secondNumber = 12;
+
+function sum(firstNumber, secondNumber){
+    let result = firstNumber + secondNumber;
+    return result;
+}
+console.log(sum(firstNumber, secondNumber));
